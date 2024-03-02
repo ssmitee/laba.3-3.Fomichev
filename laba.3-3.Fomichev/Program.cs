d9831ec90b37be47a46a37c65d55d026c7970cb9
ssmitee <144239782+ssmitee@users.noreply.github.com>
2024-03-02 11:06:47 +0200

    Добавьте файлы проекта.

---
+++ laba.3-3.Fomichev/Program.cs
@@ -0,0 +1,24 @@
+﻿//8 вариант
+using System;
+
+class SeriesSum
+{
+    static void Main()
+    {
+        double sum = 0;
+        double x;
+        int n;
+        Console.Write("Введите значение x: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите количество членов ряда n: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += Math.Pow(x, i) * Math.Sin(i * Math.PI / 4);
+        }
+
+        Console.WriteLine("Сумма ряда равна: " + sum);
+    }
+}
